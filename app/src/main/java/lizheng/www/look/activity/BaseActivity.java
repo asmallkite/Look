bbb76d8450c64c5b660673dd71692908e8c587ed
lizheng <lixiaozheng.good@163.com>
2016-09-15 14:53:03 +0800

    add baseAt

---
+++ app/src/main/java/lizheng/www/look/activity/BaseActivity.java
@@ -0,0 +1,16 @@
+package lizheng.www.look.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 10648 on 2016/9/15 0015.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
